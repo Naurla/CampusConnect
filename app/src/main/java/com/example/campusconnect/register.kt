b2c6254f51931d47f1d28b8edf3724f121e1d94b
Naurla <josenolangamaliel123@gmail.com>
2025-11-19 21:55:39 +0800

    register

---
+++ app/src/main/java/com/example/campusconnect/register.kt
@@ -0,0 +1,27 @@
+package com.example.campusconnect
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.campusconnect.databinding.ActivityRegisterBinding
+
+
+class Register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(binding.root.id)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
