0797eaf7ac06ae6f0146f0846478224a3933214c
KImdokjers <joebertsintoy1@gmail.com>
2025-11-20 21:26:17 +0800

    error

--- app/src/main/java/com/example/campusconnect/Login.kt
+++ app/src/main/java/com/example/campusconnect/Login.kt
@@ -7,16 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.campusconnect.databinding.ActivityLoginBinding
 import com.example.campusconnect.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Handle window insets
